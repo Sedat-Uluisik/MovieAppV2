dc69c10fa3c4bad97025530401dfe5e6384919ad
Sedat Uluışık <sedatuluisikk@gmail.com>
2024-03-20 18:55:59 +0300

    Create project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.sedat.movieappv2"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.sedat.movieappv2"
         minSdk = 23
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
